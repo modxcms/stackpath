fc92b4cb47e01e5165512e09c8f5b3cd20d11f3b
Garry Nutting <garry@modx.com>
2017-07-02 02:16:01 +0100

    Minor fix to labels in the setup options

--- _build/setup.options.php
+++ _build/setup.options.php
@@ -3,31 +3,31 @@ $output = '';
 switch ($options[xPDOTransport::PACKAGE_ACTION]) {
     case xPDOTransport::ACTION_INSTALL:
         $output = '
-            <label for="scdn_consumer_key">Consumer Key</label>
+            <label for="consumer_key">Consumer Key</label>
             <input type="text" name="consumer_key" id="scdn_consumer_key" width="300" value="" />
             <br /><br />
 
-            <label for="scdn_consumer_secret">Consumer Secret</label>
+            <label for="consumer_secret">Consumer Secret</label>
             <input type="text" name="consumer_secret" id="scdn_consumer_secret" width="300" value="" />
             <br /><br />
 
-            <label for="scdn_zone_id">Site ID</label>
+            <label for="zone_id">Site ID</label>
             <input type="text" name="zone_id" id="scdn_zone_id" width="300" value="" />
             <br /><br />
 
-            <label for="scdn_default_cdn_url">Default CDN URL (<i>site_name</i>-<i>company_alias</i>.stackpathdns.com)</label>
+            <label for="default_cdn_url">Default CDN URL (<i>site_name</i>-<i>company_alias</i>.stackpathdns.com)</label>
             <input type="text" name="default_cdn_url" id="scdn_default_cdn_url" width="300" value="" />
             <br /><br />
 
-            <label for="scdn_url_preview_param">URL Preview Parameter</label>
+            <label for="url_preview_param">URL Preview Parameter</label>
             <input type="text" name="url_preview_param" id="scdn_url_preview_param" width="300" value="guid" />
             <br /><br />
 
-            <label for="scdn_enabled">Enable StackPath</label>
+            <label for="enabled">Enable StackPath</label>
             <input type="radio" name="enabled" value="1" checked="checked">Yes<br />
             <input type="radio" name="enabled" value="0">No
 
-            <label for="scdn_enabled">Install Default Rules</label>
+            <label for="rules">Install Default Rules</label>
             <input type="radio" name="rules" value="1" checked="checked">Yes<br />
             <input type="radio" name="rules" value="0">No
 
